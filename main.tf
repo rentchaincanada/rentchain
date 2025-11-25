f2d598b1a4071d11e153427f436f0b3274cac38f
rentchaincanada <rentchain.ca@gmail.com>
2025-11-25 13:29:56 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,3 +12,23 @@ output "reports_bucket_name" {
   value = google_storage_bucket.reports.name
 }
 
+resource "google_storage_bucket" "audit_logs" {
+  name          = "${var.project_id}-audit-logs-bucket"
+  location      = "NORTHAMERICA-NORTHEAST1"
+  storage_class = "STANDARD"
+
+  uniform_bucket_level_access = true
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 365  # keep audit logs for 1 year
+    }
+  }
+}
+
+output "audit_logs_bucket_name" {
+  value = google_storage_bucket.audit_logs.name
+}
