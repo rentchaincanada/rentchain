45ceb0847e46492e9080513feccbea7cf12786d1
rentchaincanada <rentchain.ca@gmail.com>
2025-11-25 12:25:04 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
-output "gcp_project" {
-  value = var.project_id
+// Simple first resource: a storage bucket for future reports / exports.
+
+resource "google_storage_bucket" "reports" {
+  name          = "${var.project_id}-reports-bucket"
+  location      = "NORTHAMERICA-NORTHEAST1"  // Montréal region (Canada)
+  storage_class = "STANDARD"
+
+  uniform_bucket_level_access = true
 }
+
+output "reports_bucket_name" {
+  value = google_storage_bucket.reports.name
+}
+
