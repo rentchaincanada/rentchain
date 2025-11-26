ee0ea40c1f5d30fb931f633cd65bd42628855023
rentchaincanada <rentchain.ca@gmail.com>
2025-11-25 22:42:01 -0400

    Update firestore.tf

--- firestore.tf
+++ firestore.tf
@@ -3,18 +3,3 @@ resource "google_project_service" "firestore" {
   service            = "firestore.googleapis.com"
   disable_on_destroy = false
 }
-
-# Firestore database in Native mode
-resource "google_firestore_database" "primary" {
-  name        = "(default)"
-  project     = var.project_id
-  location_id = var.region
-
-  type = "FIRESTORE_NATIVE"
-
-  concurrency_mode = "OPTIMISTIC"
-
-  depends_on = [
-    google_project_service.firestore
-  ]
-}
