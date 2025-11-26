c533653406c393aa59344c9bcc6d503ca131e2d0
rentchaincanada <rentchain.ca@gmail.com>
2025-11-25 21:57:43 -0400

    Update cloudrun.tf

--- cloudrun.tf
+++ cloudrun.tf
@@ -17,3 +17,47 @@ resource "google_artifact_registry_repository" "api_repo" {
 
   description = "Artifact Registry repository for Rentchain landlord API containers"
 }
+
+# --------------------------------------------------------
+# Cloud Run service for the landlord API
+# --------------------------------------------------------
+
+locals {
+  api_image = "northamerica-northeast1-docker.pkg.dev/${var.project_id}/rentchain-api/rentchain-api:latest"
+}
+
+resource "google_cloud_run_service" "landlord_api" {
+  name     = "rentchain-landlord-api"
+  location = var.region
+
+  template {
+    spec {
+      containers {
+        image = local.api_image
+
+        ports {
+          name           = "http1"
+          container_port = 8080
+        }
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+
+  depends_on = [
+    google_project_service.run,
+    google_artifact_registry_repository.api_repo
+  ]
+}
+
+# Public now (we can tighten later)
+resource "google_cloud_run_service_iam_member" "landlord_api_public" {
+  location = google_cloud_run_service.landlord_api.location
+  service  = google_cloud_run_service.landlord_api.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
