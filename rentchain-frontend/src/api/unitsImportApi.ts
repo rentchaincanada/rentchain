4349930498bd043fb54f8ecdac3307e45844bc7c
rentchaincanada <rentchain.ca@gmail.com>
2026-01-05 01:51:02 -0400

    Update unitsImportApi.ts

--- rentchain-frontend/src/api/unitsImportApi.ts
+++ rentchain-frontend/src/api/unitsImportApi.ts
@@ -2,7 +2,7 @@ import api from "./client";
 
 export async function importUnitsCsv(propertyId: string, csvText: string) {
   const res = await api.post(
-    `/properties/${propertyId}/units/import`,
+    `/api/properties/${propertyId}/units/import`,
     { csvText },
     { headers: { "Content-Type": "application/json" } }
   );
