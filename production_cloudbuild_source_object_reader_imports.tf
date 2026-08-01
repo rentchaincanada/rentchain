28e46bfbf7a41e971eee2992d7cdde0d80ded28f
rentchaincanada <admin@rentchain.ai>
2026-08-01 05:37:46 -0300

    infra: import Cloud Build source reader IAM

---
+++ production_cloudbuild_source_object_reader_imports.tf
@@ -0,0 +1,9 @@
+import {
+  to = google_project_iam_custom_role.production_cloudbuild_source_object_reader
+  id = "projects/project-0d9658de-af29-4dc0-a99/roles/productionCloudBuildSourceObjectReader"
+}
+
+import {
+  to = google_storage_bucket_iam_member.production_cloudbuild_source_object_reader
+  id = "b/project-0d9658de-af29-4dc0-a99_cloudbuild projects/project-0d9658de-af29-4dc0-a99/roles/productionCloudBuildSourceObjectReader serviceAccount:rentchain-cloudbuild-builder@project-0d9658de-af29-4dc0-a99.iam.gserviceaccount.com"
+}
